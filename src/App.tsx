7b5f74477ebff90407f746eb0a112037d7262320
Abideen <jrshittu@gmail.com>
2023-11-10 18:37:53 +0100

    testing TS types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './App.css';
 
+let name: string;
+let age: number;
+let isHome: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+age = 23;
+name = "Abideen"
+isHome = false;
+
 function App() {
   return (
     <div className="App">
